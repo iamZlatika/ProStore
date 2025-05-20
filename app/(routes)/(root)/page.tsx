b59656d14ef34d69fe95f98444deb90e0deabcdb
iamzlatika <mazurovazv@gmail.com>
2025-05-20 20:54:20 +0300

    Add carousel component

--- app/(routes)/(root)/page.tsx
+++ app/(routes)/(root)/page.tsx
@@ -1,10 +1,17 @@
+import ProductCarousel from '@/components/shared/product/product-carousel';
 import ProductList from '@/components/shared/product/product-list';
-import { getLatestProducts } from '@/lib/actions/product.actions';
+import { getLatestProducts, getFeaturedProducts } from '@/lib/actions/product.actions';
 
 const HomePage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
-  return <ProductList data={latestProducts} title="Newest Arrivals" />;
+  return (
+    <>
+      {featuredProducts.length > 0 && <ProductCarousel data={featuredProducts} />}
+      <ProductList data={latestProducts} title="Newest Arrivals" />;
+    </>
+  );
 };
 
 export default HomePage;
